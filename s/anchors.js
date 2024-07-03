2ebb1ca5633f51ad8a00b3614380257dee178082
cesar <git@ferrad.as>
2024-07-03 21:43:47 +0100

    Update anchors script to be accessible
    Hide # symbol from screen readers.

--- s/anchors.js
+++ s/anchors.js
@@ -1,11 +1,13 @@
 (function() {
   var hs = document.querySelectorAll("h2,h3");
   for (var h of hs) {
+    var s = document.createElement("span");
+    s.className = "anchor";
+    s.style = "padding-left: 8px;";
+    s.setAttribute("aria-hidden", "true");
     var l = document.createElement("a");
     l.setAttribute("href", "#" + h.id);
     l.textContent = "#";
-    l.className = "anchor";
-    l.style = "padding-left: 8px; text-decoration: none;";
-    h.appendChild(l);
+    s.appendChild(l); h.appendChild(s);
   }
 })();
